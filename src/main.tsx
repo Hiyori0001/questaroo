4f0daa4ed4daf62374935751abfb0ea1ce323888
[dyad] <git@dyad.sh>
2025-10-28 13:44:52 +0530

    [dyad] Fixed context error by standardizing UserProfileProvider import path - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
-import { UserProfileProvider } from "./contexts/UserProfileContext.tsx";
+import { UserProfileProvider } from "@/contexts/UserProfileContext.tsx"; // Changed to alias import
 import { UserQuestsProvider } from "./contexts/UserQuestsContext.tsx";
 import { TeamProvider } from "./contexts/TeamContext.tsx";
-import { FriendProvider } from "./contexts/FriendContext.tsx"; // Import FriendProvider
+import { FriendProvider } from "./contexts/FriendContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="system" attribute="class">
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
       <UserProfileProvider>
         <UserQuestsProvider>
           <TeamProvider>
-            <FriendProvider> {/* Wrap App with FriendProvider */}
+            <FriendProvider>
               <App />
             </FriendProvider>
           </TeamProvider>
