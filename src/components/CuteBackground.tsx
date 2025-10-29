ff3cd161e908530fee636c860c0b54cb5400646e
[dyad] <git@dyad.sh>
2025-10-29 11:12:46 +0530

    [dyad] Attempted to fix the `tsparticles` import error by changing the `loadFull` path to `tsparticles/all`. - wrote 1 file(s)

--- src/components/CuteBackground.tsx
+++ src/components/CuteBackground.tsx
@@ -2,7 +2,7 @@
 
 import React, { useCallback } from "react";
 import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles/full"; // Reverted import path to 'tsparticles/full'
+import { loadFull } from "tsparticles/all"; // Changed import path to 'tsparticles/all'
 import type { Engine } from "tsparticles-engine";
 
 interface CuteBackgroundProps {
