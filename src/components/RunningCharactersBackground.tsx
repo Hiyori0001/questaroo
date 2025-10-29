4fad498a8b63c5f3696149321ec2dbb693ba3202
[dyad] <git@dyad.sh>
2025-10-29 12:30:30 +0530

    [dyad] Fixed icon import for running characters - wrote 1 file(s)

--- src/components/RunningCharactersBackground.tsx
+++ src/components/RunningCharactersBackground.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Rabbit, Squirrel, Fox, Bird } from "lucide-react";
+import { Rabbit, Squirrel, Cat, Bird } from "lucide-react"; // Changed Fox to Cat
 import { cn } from "@/lib/utils";
 
 interface CharacterProps {
@@ -47,7 +47,7 @@ const RunningCharactersBackground: React.FC = () => {
     <div className="absolute inset-0 overflow-hidden pointer-events-none">
       <Character icon={Rabbit} top="15%" animationDuration="25s" animationDelay="0s" direction="left-to-right" size="h-10 w-10" color="text-pink-500 dark:text-pink-400" />
       <Character icon={Squirrel} top="30%" animationDuration="30s" animationDelay="5s" direction="right-to-left" size="h-9 w-9" color="text-orange-500 dark:text-orange-400" />
-      <Character icon={Fox} top="50%" animationDuration="20s" animationDelay="10s" direction="left-to-right" size="h-12 w-12" color="text-red-500 dark:text-red-400" />
+      <Character icon={Cat} top="50%" animationDuration="20s" animationDelay="10s" direction="left-to-right" size="h-12 w-12" color="text-red-500 dark:text-red-400" />
       <Character icon={Bird} top="70%" animationDuration="35s" animationDelay="15s" direction="right-to-left" size="h-8 w-8" color="text-blue-500 dark:text-blue-400" />
       <Character icon={Rabbit} top="85%" animationDuration="28s" animationDelay="20s" direction="left-to-right" size="h-10 w-10" color="text-purple-500 dark:text-purple-400" />
     </div>
